00c76228eb12d934b9fc30d4636b634f8e48a576
Emmanue Korir <ekipyegon@co-opbank.co.ke>
2021-10-06 09:33:46 +0300

    Added Fix

--- pages/FormPopup.js
+++ pages/FormPopup.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useRef, useState }  from 'react';
-import useHook from './hook';
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import React, { useRef, useState }  from 'react';
+// import useHook from './hook';
 
-export default function Formpopup() {
-  const {handleClick,isActive, setActive} = useHook();
-    return(
+// export default function Formpopup() {
+//   const {handleClick,isActive, setActive} = useHook();
+//     return(
    
-    )
-}
+//     )
+// }
