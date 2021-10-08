812eb417b6225317ba665f9022db3b7bd72ff12b
Victor Makokha <vmakokha@sendyit.com>
2021-10-08 15:16:01 +0300

    setup socket io and change next to ssr

--- pages/Landing.js
+++ pages/Landing.js
@@ -1,108 +1,138 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState,useRef, useEffect } from "react";
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState, useRef, useEffect } from 'react';
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
 
 function Countet() {
-    const countDownDate = new Date("Oct 1, 2021 15:37:25").getTime();
-     
-      var now = new Date().getTime();
-      let difference = countDownDate - now;
-      let days = Math.floor(difference/(1000*60*60*24));
-      let hours = Math.floor((difference % (1000*60*60*24)) / (1000*60*60));
-      let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-      var Timer = days  + 'Days' + hours + "Hours" + minutes + "Minutes" + seconds + 'Seconds';
+  const countDownDate = new Date('Oct 1, 2021 15:37:25').getTime();
 
-      // console.log(Timer)
-      // return <h1> {Timer}</h1>;
-    // }, 1000);
-    const [count, setCount] = useState(0);
-      useInterval(() => {
-        // Your custom logic here
-        setCount(seconds, minutes );
-      }, 1000);
-  return <h1>   {days + ' days'} {hours +' hrs'} {minutes + ' min'} {seconds + " s"} </h1>;
-  
+  var now = new Date().getTime();
+  let difference = countDownDate - now;
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  var Timer =
+    days + 'Days' + hours + 'Hours' + minutes + 'Minutes' + seconds + 'Seconds';
 
+  // console.log(Timer)
+  // return <h1> {Timer}</h1>;
+  // }, 1000);
+  const [count, setCount] = useState(0);
+  useInterval(() => {
+    // Your custom logic here
+    setCount(seconds, minutes);
+  }, 1000);
+  return (
+    <h1>
+      {' '}
+      {days + ' days'} {hours + ' hrs'} {minutes + ' min'} {seconds + ' s'}{' '}
+    </h1>
+  );
 
   function useInterval(callback, delay) {
-      const savedCallback = useRef();
-      // Remember the latest function.
-      useEffect(() => {
-        savedCallback.current = callback;
-      }, [callback]);
+    const savedCallback = useRef();
+    // Remember the latest function.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
 
-      // Set up the interval.
-      useEffect(() => {
-        function tick() {
-          savedCallback.current();
-        }
-        if (delay !== null) {
-          let id = setInterval(tick, delay);
-          return () => clearInterval(id);
-        }
-  }, [delay]);
-}
-      
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
 }
 
 export default function Landing() {
-
-
-      // console.log(Timer)
-      // return Timer;
+  // console.log(Timer)
+  // return Timer;
+  useEffect(() => {
+    socket.on('CONNECTION_STATUS', (message) => {
+      console.log(message);
+    });
+  }, []);
   return (
     <div>
-        <div className="landing">
-            <section className="landing_seaction_1">
-            {/* <video autoPlay muted loop className="V">
+      <div className="landing">
+        <section className="landing_seaction_1">
+          {/* <video autoPlay muted loop className="V">
               <source src="../assets/video/Vid1.mp4" type="video/mp4"/>
             </video> */}
-                    <h1 className="landing_seaction_1_title">Cleaning Time !!</h1>
-                    <span className="landing_seaction_1_span"> 
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s </span>
-                    <div className="landing_seaction_1_sub_sect"> 
-                      <div className="landing_seaction_1_sub_sect_wrapper">
-                      <span className="landing_seaction_1_sub_sect_heading"> How clean is your home ?</span>
-                        <p className="landing_seaction_1_sub_sect_p"> 
-                          Would you give us a chance to train YOU and/or YOUR Domestic 
-                          Manager to transform and sparkle up your space like pro ? </p>
-                        <span className="landing_seaction_1_sub_sect_b_text"> For only 300/- Join us on the 15th Oct From 9:30 am from transformative session </span>
-                        <span className="landing_seaction_1_sub_sect_footer_text"> Get Value for you money... </span>
-                        <button className="landing_seaction_1_sub_sect_register"> REGISTER NOW</button>
-                      </div>
-                    </div>
-            </section>
-            <section className="landing_section_2">
-
-            <div className="landing_section_2_wrapper_1">
-            <span className="landing_section_2_wrapper_1_heading"> About Event </span>
-            <p className="landing_section_2_wrapper_1_p">  
-              Would you give us a chance to train YOU and/or YOUR Domestic 
-                          Manager to transform and sparkle up your space like pro ?
+          <h1 className="landing_seaction_1_title">Cleaning Time !!</h1>
+          <span className="landing_seaction_1_span">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s{' '}
+          </span>
+          <div className="landing_seaction_1_sub_sect">
+            <div className="landing_seaction_1_sub_sect_wrapper">
+              <span className="landing_seaction_1_sub_sect_heading">
+                {' '}
+                How clean is your home ?
+              </span>
+              <p className="landing_seaction_1_sub_sect_p">
+                Would you give us a chance to train YOU and/or YOUR Domestic
+                Manager to transform and sparkle up your space like pro ?{' '}
+              </p>
+              <span className="landing_seaction_1_sub_sect_b_text">
+                {' '}
+                For only 300/- Join us on the 15th Oct From 9:30 am from
+                transformative session{' '}
+              </span>
+              <span className="landing_seaction_1_sub_sect_footer_text">
+                {' '}
+                Get Value for you money...{' '}
+              </span>
+              <button className="landing_seaction_1_sub_sect_register">
+                {' '}
+                REGISTER NOW
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="landing_section_2">
+          <div className="landing_section_2_wrapper_1">
+            <span className="landing_section_2_wrapper_1_heading">
+              {' '}
+              About Event{' '}
+            </span>
+            <p className="landing_section_2_wrapper_1_p">
+              Would you give us a chance to train YOU and/or YOUR Domestic
+              Manager to transform and sparkle up your space like pro ?
             </p>
-            </div> 
-              <div className="landing_section_2_wrapper_1">
-            <span className="landing_section_2_wrapper_1_heading"> Where ? </span>
-            <p className="landing_section_2_wrapper_1_p">  
-             Would you give us a chance to train YOU and/or YOUR Domestic 
-                          Manager to transform and sparkle up your space like pro ?500s
+          </div>
+          <div className="landing_section_2_wrapper_1">
+            <span className="landing_section_2_wrapper_1_heading">
+              {' '}
+              Where ?{' '}
+            </span>
+            <p className="landing_section_2_wrapper_1_p">
+              Would you give us a chance to train YOU and/or YOUR Domestic
+              Manager to transform and sparkle up your space like pro ?500s
             </p>
-            </div> 
-              <div className="landing_section_2_wrapper_1">
+          </div>
+          <div className="landing_section_2_wrapper_1">
             <span className="landing_section_2_wrapper_1_heading">Date </span>
-              <p className="landing_section_2_wrapper_1_p">  
-            <Countet />
+            <p className="landing_section_2_wrapper_1_p">
+              <Countet />
             </p>
-               <button className="landing_seaction_1_sub_sect_register"> BOOK NOW</button>
-          
-            </div> 
-            
-        
-               
-            </section>
-        </div>
+            <button className="landing_seaction_1_sub_sect_register">
+              {' '}
+              BOOK NOW
+            </button>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
--- pages/entry.js
+++ pages/entry.js
@@ -1,9 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Landing from './Landing'
+import Head from 'next/head';
+import Image from 'next/image';
+import Landing from './Landing';
+
 export default function Entry() {
-  return (
- 
-    <Landing />
-  )
+  return <Landing />;
 }
